a57f3dbc73b1cdf4bd8646453283e62bea305098
adeiming <imingforum@gmail.com>
2025-10-19 19:19:11 +0700

    scroll trigger

---
+++ app/pages/index.vue
@@ -0,0 +1,297 @@
+<script setup>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+const loader = ref(true)
+let smooth = null
+
+useHead({
+    title: 'Makan Mantan',
+})
+onMounted(async () => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    await nextTick()
+
+    smooth = ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 2,
+        smoothTouch: 0.1,
+        effects: true
+    })
+
+    loader.value = false
+
+    gsap.timeline()
+        .from("._logo", {
+            opacity: 0,
+            x: -50,
+            duration: 0.5,
+        })
+        .from(
+            "._link",
+            {
+                y: -20,
+                opacity: 0,
+                stagger: 0.1,
+            },
+            "<0.1"
+        )
+        .from("._a", {
+            y: -20,
+            opacity: 0,
+            stagger: 0.2,
+        })
+        .fromTo("._botol", { opacity: 0, visibility: 'hidden', zIndex: -1 }, {
+            opacity: 1,
+            zIndex: 1,
+            visibility: 'visible',
+        })
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: "#_a",
+            start: "center center",
+            end: "bottom center",
+            scrub: 1,
+        },
+    })
+        .to("._a", {
+            y: -20,
+            scale: 1.5,
+            stagger: 0.1,
+        })
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.__botol',
+            start: "top center",
+            end: "bottom center",
+            scrub: 1,
+        }
+    })
+        .from('.__botol', {
+            xPercent: -100,
+            rotate: -5,
+        })
+        .from('._b1', {
+            y: 100,
+            opacity: 0
+        }, '<0.5')
+        .fromTo("._botol", {
+            top: '55%',
+            left: '100%',
+            xPercent: -30,
+            rotate: -10
+        }, {
+            left: '100%',
+            xPercent: -90,
+            opacity: 1,
+            rotate: 0
+        }, '<')
+        .to("._botol", {
+            rotate: 10,
+        })
+        .to("._botol", {
+            rotate: 0
+        })
+        .to("._botol", {
+            rotate: -10
+        })
+        .to('.__botol', {
+            xPercent: 100,
+            rotate: 5,
+        })
+        .to("._botol", {
+            left: '100%',
+            xPercent: -50
+        }, '<0.5')
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.__profil',
+            start: "top center",
+            end: "bottom center",
+            scrub: 1,
+        }
+    })
+        .from('.__profil', {
+            xPercent: 200,
+            rotate: 5,
+        })
+        .from('._b2', {
+            y: 100,
+            opacity: 0,
+        })
+        .to('._b2', {
+            y: 0,
+        })
+        .to('._b2', {
+            y: 0,
+        })
+        .to('.__profil', {
+            xPercent: -200,
+            rotate: -5,
+        })
+
+    const botol = gsap.timeline({
+        paused: true,
+    }).fromTo('._botol', {top: '55%'},  {
+        top: '60%',
+        yoyo: true,
+        duration: 3,
+        repeat: -1,
+        ease: 'none'
+    })
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.__kontak',
+            start: "top center",
+            end: "center center",
+            scrub: 1,
+        }
+    })
+    .from('.__kontak', {
+        rotate: -5,
+        xPercent: -100,
+    })
+    .from(`._b3`, {
+        yPercent: 100,
+        opacity: 0
+    })
+    .to('._botol', {
+        left: '50%',
+        xPercent: -50,
+        rotate: 0,
+        filter: 'saturate(200%)',
+        scale: .8,
+        onComplete: () => botol.play(),
+        onReverseComplete: () => botol.pause(),
+    })
+
+});
+
+const navigate = (e) => {
+    e.preventDefault()
+    smooth.scrollTo(e.target.hash, true, 'top top');
+}
+</script>
+
+<template>
+    <div class="backdrop-blur-lg fixed inset-0 z-20 bg-white/10" v-if="loader"></div>
+    <img src="/botol.png" class="_botol size-60 fixed invisible z-0 saturate-100" />
+    <div class="bg-amber-50 min-h-screen" id="smooth-wrapper">
+        <header class="bg-amber-400/70 backdrop-blur flex gap-2 font-semibold text-amber-900 py-2 shadow-lg justify-between p-0 sm:px-10 top-0 z-10 sticky">
+            <a href="#" class="p-2 _logo">AZIMA</a>
+            <nav class="flex">
+                <a href="#tentang" @click="navigate" class="_link p-2">Tentang</a>
+                <a href="#profil" @click="navigate" class="_link p-2">Profil</a>
+                <a href="#kontak" @click="navigate" class="_link p-2">Kontak</a>
+            </nav>
+        </header>
+        <div class="overflow-hidden" id="smooth-content">
+            <UContainer class="pb-25">
+                <main class="text-lg text-black">
+                    <section class="min-h-screen gap-2 flex justify-center items-center flex-col bg-amber-50 text-shadow-lg" id="_a">
+                        <h1 class="_a font-semibold text-7xl sm:text-9xl text-center text-amber-600">
+                            Makan Malam
+                        </h1>
+                        <h2 class="_a font-semibold text-6xl sm:text-8xl text-center text-amber-500">
+                            Bersama Mantan
+                        </h2>
+                        <img src="/baso.png" class="_a size-60" />
+                    </section>
+                    <section class="bg-white rounded-xl relative overflow-hidden p-10 __botol" id="tentang">
+                        <div class="sticky top-1 flex z-2">
+                            <h1 class="text-xl font-semibold block bg-amber-600/20 backdrop-blur-lg text-amber-800 p-4">Tentang</h1>
+                        </div>
+                        <div class="text-center _b1">
+                            <div class="space-y-5 py-10 md:py-30">
+                                <p class="font-semibold text-xl">
+                                    “Kisah di Balik Meja Ini”
+                                </p>
+
+                                <div>
+                                    Malam, lampu redup, dan denting sendok di gelas —
+                                    begitulah semua ini dimulai.
+                                </div>
+
+                                <div>
+                                    Kami percaya, makanan punya cara sendiri untuk bercerita.
+                                    Bahwa setiap suapan bisa membawa kita ke masa lalu,
+                                    ke tawa yang dulu sederhana, ke momen yang dulu terasa utuh.
+                                </div>
+
+                                <div>
+                                    “Makan Malam Bersama Mantan” lahir dari ide sederhana:
+                                    bagaimana jika kenangan pahit dan manis bisa disatukan di atas meja yang sama,
+                                    bukan untuk membuka luka, tapi untuk menutupnya dengan senyum.
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="bg-amber-100 rounded-xl relative overflow-hidden p-10 __profil" id="profil">
+                        <div class="sticky top-1 flex z-2">
+                            <h1 class="text-xl font-semibold block bg-amber-600/20 backdrop-blur-lg text-amber-800 p-4">Profil</h1>
+                        </div>
+                        <div class="text-center z-1 _b2">
+                            <div class="space-y-5 py-10 md:py-30">
+                                <p class="font-semibold text-xl">
+                                    “Siapa di Balik Kisah Ini”
+                                </p>
+
+                                <p>
+                                    Kami adalah orang-orang yang percaya bahwa perpisahan bukan akhir,
+                                    dan bahwa kenangan bisa jadi sesuatu yang indah bila disajikan dengan hangat.
+                                </p>
+
+                                <p>
+                                    Di balik meja dan lilin, ada tim kecil yang mencintai suasana,
+                                    menghargai kenangan, dan memahami arti keheningan di antara dua orang yang pernah saling mencinta.
+                                </p>
+
+                                <p>
+                                    Kami bukan sekadar penyaji makanan,
+                                    kami perangkai suasana, pengantar rasa, dan pencipta momen yang tidak bisa dipesan ulang.
+                                </p>
+
+                            </div>
+                        </div>
+                    </section>
+                    <section class="bg-white rounded-xl relative overflow-hidden p-10 __kontak" id="kontak">
+                        <div class="sticky top-1 flex z-2">
+                            <h1 class="text-xl font-semibold block bg-amber-600/20 backdrop-blur-lg text-amber-800 p-4">Kontak</h1>
+                        </div>
+                        <div class="text-center z-1 _b3">
+                            <div class="space-y-5 py-10 md:py-30">
+                                <p class="font-semibold text-xl">
+                                    “Ingin Mengulang Cerita?”
+                                </p>
+
+                                <p>
+                                    Mungkin malam ini bukan untuk kembali,
+                                    tapi untuk berdamai dengan masa lalu.
+                                </p>
+
+                                <p>
+                                    Jika kamu ingin memesan meja, mengirim pesan, atau sekadar berbagi cerita —
+                                    kami selalu siap mendengarkan, dengan secangkir teh hangat dan lampu temaram yang menenangkan.
+                                </p>
+
+                                <p>
+                                    Hubungi kami, bukan untuk mengulang yang telah berlalu,
+                                    tapi untuk mengingat bahwa kenangan pun bisa disajikan dengan indah.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="px-10 pt-30 pb-5 z-50 relative text-center font-bold">
+                        &nbsp; 2025 AZIMA CORP
+                    </section>
+                </main>
+            </UContainer>
+        </div>
+    </div>
+</template>
---
+++ app/pages/scroll-trigger.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="min-h-screen bg-sky-50 text-black" id="wrapper">
+    <div id="content">
+        <UContainer class="space-y-5 py-10">
+            <section class="min-h-screen bg-sky-100 p-10 rounded-xl flex flex-col justify-between relative">
+                <h1 class="font-semibold text-xl">Normal</h1>
+                <div ref="normal">
+                    <div class="bg-sky-600 size-20 _normal"></div>
+                </div>
+            </section>
+            <section class="min-h-screen bg-sky-200 p-10 rounded-xl flex flex-col justify-between relative">
+                <h1 class="font-semibold text-xl">Pin</h1>
+                <div class="space-y-50">
+                    <div class="bg-sky-600 mx-auto size-20 _pin"></div>
+                    <div class="bg-sky-900 mx-auto size-20"></div>
+                </div>
+            </section>
+            <section class="min-h-screen bg-sky-100 p-10 rounded-xl relative _timeline-base" ref="timeline-base">
+                <h1 class="font-semibold text-xl">Timeline</h1>
+                <div class="mt-10">
+                    <div class="bg-sky-600 size-20 _timeline"></div>
+                </div>
+            </section>
+            <section class="min-h-screen bg-sky-200 p-10 rounded-xl flex flex-col justify-between relative _self-create-base">
+                <h1 class="font-semibold text-xl">Self Create Toggle Class</h1>
+                <div class="flex-1 flex justify-center items-center">
+                    <div class="bg-sky-600 size-20 _self-create transition-all duration-1000"></div>
+                </div>
+            </section>
+            <section class="min-h-screen bg-sky-100 p-10 rounded-xl relative">
+                <h1 class="font-semibold text-xl">Marker</h1>
+                <div class="mt-10 space-y-4">
+                    <p>
+                        Lorem ipsum dolor sit amet <span>consectetur adipisicing elit.</span>  Saepe qui velit eveniet assumenda
+                        atque labore maiores, excepturi, dolore, laboriosam praesentium corrupti eos. Voluptatibus vel fugit,
+                        quam unde quisquam doloribus voluptatum quod nostrum corrupti aliquam, deleniti, ea corporis numquam maxime
+                    </p>
+                    <p>
+                        nemo necessitatibus delectus repellat beatae culpa. Suscipit quasi a qui aliquid similique quam veniam voluptas
+                        enim placeat tempore porro doloribus  <span>hic cupiditate sed dignissimos perferendis</span> nemo consequuntur,
+                        commodi accusantium aspernatur rerum aperiam incidunt aut? Quo tempora veniam explicabo error, magnam ullam,
+                    </p>
+                    <p>
+                        ex impedit nemo temporibus aliquam rerum tempore fuga quis, labore nihil natus architecto aperiam! Molestias sunt
+                        cupiditate dolore voluptatem quos nulla optio eligendi aspernatur soluta maiores officia enim cum iusto, ullam
+                        iure obcaecati voluptatum nobis. <span>Porro odit, saepe tempora debitis illum sit quidem,</span>  temporibus animi beatae
+                    </p>
+                    <p>
+                        sequi ullam quos qui. Molestiae odit  <span>illo sunt, odio quod alias. Commodi id totam esse soluta
+                        possimus ex eius cumque, et ea labore sapiente sequi est rem suscipit</span> omnis modi! Nulla magnam voluptates ab
+                        voluptas fugiat doloremque unde, velit perspiciatis obcaecati veniam impedit nihil in eaque exercitationem numquam sed,
+                    </p>
+                    <p>
+                        deleniti excepturi deserunt. Cupiditate asperiores magnam non iste voluptatem, voluptates est vitae soluta assumenda
+                        inventore, deserunt molestias totam quaerat  <span>doloribus cumque recusandae omnis nostrum. Fuga minima iusto debitis expedita,
+                        explicabo, tempora mollitia aliquam veniam voluptatum</span> veritatis eveniet recusandae reprehenderit corrupti nesciunt nam animi eum?
+                    </p>
+                    <p>
+                        Repellat architecto repudiandae id quaerat ad optio eveniet. Eum nostrum corrupti reiciendis saepe quam vitae eligendi molestias.
+                        Neque, fugiat officia  <span>atque unde laboriosam harum corrupti minima sapiente vero alias sunt nulla adipisci amet ea magnam </span>nesciunt,
+                        vel possimus impedit ratione doloribus et. Cumque fugit numquam et illo? Error, voluptate asperiores similique, officiis laudantium
+                    </p>
+                    <p>
+                        iusto id fuga suscipit qui harum pariatur incidunt labore? Distinctio, ad eum nesciunt inventore corrupti quaerat velit pariatur iure facere
+                        quo eligendi optio deserunt necessitatibus enim expedita, fugiat nam voluptatibus alias nihil! Alias, numquam eaque itaque sed aliquam inventore
+                        distinctio quibusdam ducimus. <span>Fuga, illo natus illum delectus ad voluptas magnam? Nesciunt, adipisci.</span>  Eveniet hic nobis tempore adipisci fugiat
+                    </p>
+
+                    <p>
+                        facilis et vel iste nisi veniam aspernatur <span>laborum aut incidunt maiores, nam mollitia pariatur voluptas ad eligendi temporibus quam natus id deserunt.
+                        Sit debitis soluta nulla dicta et? Minima rem odio repellat nobis illum id quas, debitis voluptatum accusantium unde placeat, delectus porro
+                        quibusdam eveniet, molestiae optio. Culpa, accusantium. Architecto aliquam inventore</span> possimus! Sapiente reprehenderit tempora labore iure,
+                    </p>
+
+                    <p>
+                        provident repudiandae. Temporibus cum ad ex nostrum, expedita in est dolore accusantium earum repellendus pariatur corporis nesciunt quam illum placeat,
+                        quae, enim iure veritatis <span>animi perspiciatis. Delectus quos assumenda ut hic tempore odio impedit neque, deserunt maxime. Voluptatum repellendus
+                        a quam iure odio, voluptas eligendi nihil</span> inventore cupiditate saepe doloremque, exercitationem, ipsa temporibus quidem. Assumenda delectus cumque
+                    </p>
+                    <p>
+                        ipsam numquam laborum, <span>fugit officia enim dicta iusto illo alias eius ab veritatis est. Voluptatum, illo voluptatem asperiores assumenda,
+                        necessitatibus repellat soluta ab, </span>aperiam numquam quam iste. Porro rerum fugiat sit. Natus magnam, autem aut dolores corrupti
+                        odio aperiam, voluptas ab id quo reprehenderit consequatur consequuntur nam! Hic eius expedita autem alias dolor distinctio minus.
+                    </p>
+                </div>
+            </section>
+            <section class="min-h-[50vh] font-semibold text-xl bg-sky-200 p-10 rounded-xl flex justify-center items-center relative">
+                &copy; AZIMA CORP
+            </section>
+        </UContainer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+    import { gsap } from 'gsap'
+    import { ScrollTrigger } from 'gsap/ScrollTrigger'
+    import { ScrollSmoother } from 'gsap/ScrollSmoother'
+
+    useHead({
+        title: 'Scroll Trigger',
+    })
+
+    const normal = useTemplateRef('normal')
+    const timelineBase = useTemplateRef('timeline-base')
+
+    onMounted(async () => {
+        gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+        await nextTick()
+
+        ScrollSmoother.create({
+            wrapper: "#wrapper",
+            content: "#content",
+            smooth: 2,
+            smoothTouch: 0.1
+        })
+
+        gsap.fromTo('._normal', {
+            x: 0,
+            borderRadius: 0
+        }, {
+            x: () => `+=${Number(normal.value?.offsetWidth) - 80}`,
+            borderRadius: '20%',
+            rotate: 90,
+            scrollTrigger: {
+                trigger: '._normal',
+                start: 'top center',
+                end: 'bottom center',
+                scrub: 1,
+                // markers: true
+            }
+        })
+
+        gsap.fromTo('._pin', {
+            scale: 1.5,
+            borderRadius: '20%',
+        }, {
+            scale: 1,
+            borderRadius: 0,
+            scrollTrigger: {
+                trigger: '._pin',
+                start: 'top center',
+                end: '+=200 center',
+                pin: true,
+                pinSpacing: false,
+                scrub: 1,
+                // markers: true
+            }
+        })
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '._timeline-base',
+                start: '20% center',
+                end: '80% center',
+                scrub: 1,
+                // markers: true
+            }
+        })
+        .fromTo('._timeline', {
+            x: 0,
+        }, {
+            x: () => `+=${Number(normal.value?.offsetWidth) - 80}`,
+            borderRadius: '20%',
+            rotate: 90,
+        })
+        .to('._timeline', {
+            y: () => `+=${Number(timelineBase.value?.offsetHeight) - 250}`,
+            rotate: 180,
+        })
+        .to('._timeline', {
+            x: 0,
+            borderRadius: 0,
+            rotate: 360,
+        })
+
+        ScrollTrigger.create({
+            trigger: '._self-create-base',
+            start: '20% center',
+            end: '80% center',
+            toggleClass: {
+                targets: '._self-create',
+                className: 'active'
+            },
+            // markers: true
+        })
+
+        gsap.utils.toArray('span').forEach((el: any) => {
+            ScrollTrigger.create({
+                trigger: el,
+                start: 'top center',
+                toggleClass: 'marked',
+                // markers: true
+            })
+        })
+    })
+</script>
+
+<style scoped>
+    span {
+        background-image: linear-gradient(yellow, yellow);
+        background-size: 0% 100%;
+        background-repeat: no-repeat;
+        transition: background-size 1s ease-in-out;
+    }
+
+    span.marked {
+        background-size: 100% 100%;
+    }
+</style>
